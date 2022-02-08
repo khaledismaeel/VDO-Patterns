d6f8dc65cd9a0814f275962727057cc041b38bb3
Ildar1 <ildarnn22@gmail.com>
2022-02-08 13:58:32 +0300

    User Rights Assignment requirement

---
+++ src/patterns/UserRightsAssignRequirements.java
@@ -0,0 +1,9 @@
+package patterns;
+
+abstract public class UserRightsAssignRequirements extends LocalPolicyRequirement {
+    @Override
+    protected String getCategory() {
+        return "User Rights Assignment";
+    }
+}
+
